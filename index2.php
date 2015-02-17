71c78cca62e93d3dd1b2a4a75b7debe0f5e2d382
AirisaAtvare <airisa.atvare@gmail.com>
2015-02-17 16:04:10 +0200

    second page

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <link rel="stylesheet" type="text/css" href="test.css" />
+     <title>Index 2</title>
+   </head>
+   <body>
+     <h1>Hello World 2</h1>
+     <image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRifD_A5qz__k8tEhjq9ya1F4QdJD1ikZfBlZYxkq6yCFOtvHH2R_PtI5E" alt="hamster picture"/>
+     <a href="index.php">index</a>
+
+   </body>
+</html>
