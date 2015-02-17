1b89f92772b05782f1b6cd67cddce89b624b7bc4
AirisaAtvare <airisa.atvare@gmail.com>
2015-02-17 16:03:04 +0200

    colors

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
    <head>
+     <link rel="stylesheet" type="text/css" href="test.css" />
      <title>Bootcamp</title>
    </head>
    <body>
      <h1>Hello World</h1>
-     <image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRifD_A5qz__k8tEhjq9ya1F4QdJD1ikZfBlZYxkq6yCFOtvHH2R_PtI5E" alt="hamster picture"/>
-     <a href="http://9gag.com/">enjoy 9gag</a>
-     
+     <image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRifD_A5qz__k8tEhjq9ya1F4QdJD1ikZfBlZYxkq6yCFOtvHH2R_PtI5E" alt="hamster picture"/><BR>
+     <a href="index2.php">index2</a><br>
+     <FORM action="" method="post">
+     <LABEL for="firstname">First name: </LABEL>
+     <INPUT type="text" id="firstname"><BR>
+
    </body>
 </html>
