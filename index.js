154b040ec4e4aa92893debcd8165fe766280b2eb
AirisaAtvare <airisa.atvare@gmail.com>
2015-02-17 17:21:35 +0200

    fizzbuzz

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello");
+
+for (var i = 1; i <= 20; i++) {
+   // console.log(i%3);
+   // more statements
+  if(i%3 == 0 && i%5 == 0) {
+     console.log("FizzBuzz");
+   }
+
+    else if (i%3 == 0) {
+      console.log("Fizz");
+    }
+    else if (i%5 == 0) {
+       console.log("Buzz");
+     }
+
+     else {
+       console.log(i);
+          }
+}
