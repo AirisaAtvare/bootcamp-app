6132525abcfb65085726a1e9c13e6d795ca8321b
AirisaAtvare <airisa.atvare@gmail.com>
2015-02-17 14:56:24 +0200

    added picture, link, title, h1

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-HELLO
+<!DOCTYPE html>
+<html>
+   <head>
+     <title>Bootcamp</title>
+   </head>
+   <body>
+     <h1>Hello World</h1>
+     <image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRifD_A5qz__k8tEhjq9ya1F4QdJD1ikZfBlZYxkq6yCFOtvHH2R_PtI5E" alt="hamster picture"/>
+     <a href="http://9gag.com/">enjoy 9gag</a>
+     
+   </body>
+</html>
