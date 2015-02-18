0c16cdd7a8485de7e51f398ac3022321aad52b39
AirisaAtvare <airisa.atvare@gmail.com>
2015-02-18 17:48:54 +0200

    viskas

--- index.php
+++ index.php
@@ -5,13 +5,37 @@
      <title>Bootcamp</title>
    </head>
    <body>
-     <h1>Hello World</h1>
+     <h1><?php echo "some random text"?></h1>
+
      <image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRifD_A5qz__k8tEhjq9ya1F4QdJD1ikZfBlZYxkq6yCFOtvHH2R_PtI5E" alt="hamster picture"/><BR>
      <a href="index2.php">index2</a><br>
-     <FORM action="" method="post">
-     <LABEL for="firstname">First name: </LABEL>
-     <INPUT type="text" id="firstname"><BR>
-       
+
+     <form action="saveComent.php" method="post">
+     <label for="Name">Name: </label>
+     <input type="text" id="name" name="user"><br>
+
+
+
+     <label for="Comment">Comment: </label>
+     <textarea name="comment" rows="10" cols="30">Comment</textarea>
+     <input type="submit" value="button"><br>
     <script type="text/javascript" src="index.js"></script>
+
+
+    <?php
+    $mysqli = new mysqli("localhost", "homestead", "secret", "bootcamp");
+    $result=$mysqli->query("SELECT * FROM Comments");
+      while ($row = $result->fetch_assoc()){
+        echo $row['author'];
+        echo '<br>';
+
+        echo $row['comment'];
+        echo '<hr>';
+      }
+
+
+
+
+    ?>
    </body>
 </html>
